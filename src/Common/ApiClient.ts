8981b9603a9bb69bc3e3242c7782f6d725f70474
weeger <romain.weeger@gmail.com>
2026-01-10 07:02:55 +0100

    Api client

--- src/Common/ApiClient.ts
+++ src/Common/ApiClient.ts
@@ -1,3 +1,15 @@
+export type ApiClientOptions = Readonly<{ baseUrl?: string | null }>;
+
+type NoExtra<T, U extends T> = U & Record<Exclude<keyof U, keyof T>, never>;
+
 export default class ApiClient {
-  baseUrl: string | null = null;
+  public readonly baseUrl: string | null;
+
+  private constructor(options: ApiClientOptions = {}) {
+    this.baseUrl = options.baseUrl ?? null;
+  }
+
+  static create<U extends ApiClientOptions>(options: NoExtra<ApiClientOptions, U> = {} as any) {
+    return new ApiClient(options);
+  }
 }
